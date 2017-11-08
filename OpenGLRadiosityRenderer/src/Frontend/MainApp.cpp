68e34bfa4d78f5efb0adefe6885fb2729a6e595b
Tamas Kormendi <tamas.kormendi@kcl.ac.uk>
2017-11-08 17:17:42 +0000

    Create Main entry point for GUI

---
+++ OpenGLRadiosityRenderer/src/Frontend/MainApp.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include <wx\wx.h>
+
+#include <Frontend\MainApp.h>
+
+bool MainApp::OnInit() {
+	return false;
+}
---
+++ OpenGLRadiosityRenderer/src/Frontend/MainApp.h
@@ -0,0 +1,9 @@
+#ifndef MAINAPP_H
+#define MAINAPP_H
+
+class MainApp : public wxApp {
+public:
+	virtual bool OnInit();
+};
+
+#endif
